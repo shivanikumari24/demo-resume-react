59e1ebd3bc903515e207f8e8170294def6474c5b
shivani jha <78164260+shivanikumari24@users.noreply.github.com>
2023-05-04 22:15:51 +0530

    Update Resume.js

--- src/Resume.js
+++ src/Resume.js
@@ -2,20 +2,15 @@ import '../componentCSS/resume.css'
 export default function Resume() {
     return (
         < div>
-            {/* <h1 id="heading"><center> Resume</center></h1>
-        <hr width ='80%'></hr> */}
-
             <h1 className='info1'>Shivani Jha</h1>
             <p className='info'><strong> Contact : </strong> 629900000</p>
             <p className='info'><strong> Email : </strong> shivani@gmail.com</p>
             <p className='info'><strong> Address :</strong>H No. 13 ,Dhulkoot <br></br>Dehradun,248007</p>
-            {/* <img src='profile.webp'></img> */}
 
             <hr></hr>
 
             <h3 className='obj'>CAREER OBJECTIVE</h3>
             <p className='detail'> <b>• </b>To obtain a challenging career in the IT industry and put all my efforts into the growth of the organization and have a great working environment.</p>
-
             <h3 className='obj'>EDUCATION</h3>
             <p className='detail'><b>• </b>Tula's Institute,Dehradun - Bachelor of Technology in computer science and Engineering</p>
             <p className='pree'>(2020-2024)</p>
@@ -24,7 +19,6 @@ export default function Resume() {
             <p className='detail'><b>• </b>Shivam convent, Patna — Secondary Education | CBSE </p>
             <p className='pree'>(2016-2018)</p>
             <p className='detail'><b>• </b>HTML</p>
-
 
 
             <h3 className='obj'>Technical skills</h3>
