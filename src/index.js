1decc5a58ef06fd1b04357829862cff55d71c510
shivani jha <78164260+shivanikumari24@users.noreply.github.com>
2023-05-04 22:14:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Resume, { Objective } from './mycomponent/Resume';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
