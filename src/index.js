ea485b3c4f4d892a081b707dbc4a7b4982e020f9
shivani jha <78164260+shivanikumari24@users.noreply.github.com>
2023-05-04 22:08:48 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Resume, { Objective } from './mycomponent/Resume';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Resume />
+    <Objective />
+  </>
+);
+
+ 
